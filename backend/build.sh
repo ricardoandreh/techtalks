7fa0e64aabf658e88e4a8e9c0c91a680d1a32ad6
Ricardo André <ricardo.andre.ifc@gmail.com>
2024-09-26 12:00:38 -0300

    build: render.yaml & build.sh

--- backend/build.sh
+++ backend/build.sh
@@ -3,7 +3,7 @@
 set -o errexit
 
 # Modify this line as needed for your package manager (pip, poetry, etc.)
-pip install -r requirements.txt --user
+pip install -r requirements.txt
 
 # Convert static asset files
 python3 manage.py collectstatic --no-input
