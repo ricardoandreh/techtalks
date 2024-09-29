6c226e11cc04d72fb50a9760126acf1c4412e965
Ricardo André <ricardo.andre.ifc@gmail.com>
2024-09-29 16:47:36 -0300

    build: superuser condition

--- backend/build.sh
+++ backend/build.sh
@@ -2,6 +2,9 @@
 # Exit on error
 set -o errexit
 
+# Upgrade pip
+pip install --upgrade pip
+
 # Modify this line as needed for your package manager (pip, poetry, etc.)
 pip install -r requirements.txt
 
@@ -11,5 +14,10 @@ python manage.py collectstatic --no-input
 # Apply any outstanding database migrations
 python manage.py migrate
 
-# Create your super user (check your env vars)
-python manage.py createsuperuser --no-input
+# Check if superuser exists before creating
+if
+  [ $(echo "from django.contrib.auth import get_user_model; User = get_user_model(); print(User.objects.filter(is_superuser=True).exists())" | python manage.py shell) == "False" ];
+then
+  # Create your super user (check your env vars)
+  python manage.py createsuperuser --no-input
+fi
