60e656ab1fdcf2627b459737e862b3cd2ddac39a
Ricardo André <ricardo.andre.ifc@gmail.com>
2024-10-01 09:21:31 -0300

    feat: add pinia and persisted-state

--- frontend/src/main.js
+++ frontend/src/main.js
@@ -1,10 +1,20 @@
+import { createPinia } from "pinia"
+import { createPersistedState } from "pinia-plugin-persistedstate";
 import { createApp } from "vue"
 
 import App from "./App.vue"
 import router from "./router"
 
 const app = createApp(App)
+const pinia = createPinia();
 
+pinia.use(
+createPersistedState({
+    storage: sessionStorage,
+    auto: true,
+})
+);
+app.use(pinia)
 app.use(router)
 
 app.mount("#app")
